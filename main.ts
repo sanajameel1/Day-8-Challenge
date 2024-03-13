c01ad5fbde5f488da08dea8d773ae0255043c19f
sana <161457792+sanajameel1@users.noreply.github.com>
2024-03-13 22:35:30 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,66 @@
+// //*Question 22:* Intentional Error: Try to produce an array index error in one of your programs.
+// // Correct the error before finishing.
+// //*Explain & TIP:* Array index errors happen when you try to access a position in the array that doesn’t exist. 
+// //Experimenting with this helps you understand array boundaries.
+ 
+// let friends: string[] = ["Aqsa", "Abeer ", "Sana"];
+
+// console.log(friends[3]); 
+// friends[2] = "Sana"; 
+
+
+// //*Question 23:* Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+// //*Explain & TIP:* Conditional tests check if something is true or false.
+// // Writing different tests helps you understand how conditions work in programming.
+
+//  let car = 'subaru';
+//  console.log("Is car == 'subaru'? I predict True.");
+//  console.log(car == 'subaru'); 
+//  console.log("Is car == 'toyota'? I predict False.");
+//  console.log(car == 'toyota'); 
+
+// //*Question 24:* More Conditional Tests: Expand your conditional tests to cover a wider range of comparisons.
+// //*Explain & TIP:* Experiment with different types of conditions to see how they work.
+//  //This builds a deeper understanding of logic in programming.
+
+//  console.log("Testing equality with strings:");
+//  console.log("orange" == "orange"); // True
+//  console.log("orange" == "orange"); // False
+ 
+ 
+// // Using the lower case function
+// console.log("Testing with lower case:");
+// console.log("Apple".toLowerCase() == "apple"); // True
+
+// // Numerical tests
+// console.log("Numerical tests:");
+// console.log(20 > 10); // True
+// console.log(3 < 1); // False
+
+// // Tests using "and" and "or" operators
+// console.log("Tests with 'and' and 'or':");
+// console.log(true && false); // False
+// console.log(true || false); // True
+
+// // Test whether an item is in a array
+// let fruits = ["apple", "banana", "orange"];
+// console.log("Is 'apple' in fruits?");
+// console.log(fruits.includes("apple")); // True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
